86fabaa744780f72680b9b37704d3ee09e4620ec
Jinkyu Han <hjk1019@gmail.com>
2020-01-30 23:11:54 +0900

    PostsRepository 테스트 코드 작성

---
+++ src/test/java/com/example/demo/domain/posts/PostsRepositoryTest.java
@@ -0,0 +1,54 @@
+package com.example.demo.domain.posts;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+
+import java.util.List;
+
+import com.example.demo.domains.posts.Posts;
+import com.example.demo.domains.posts.PostsRepository;
+
+import org.junit.After;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+/**
+ * PostsRepositoryTest
+ */
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PostsRepositoryTest {
+
+    @Autowired
+    PostsRepository postsRepository;
+
+    /**
+     * 청소한다
+     */
+    @After
+    public void cleanup() {
+        postsRepository.deleteAll();
+    }
+
+    @Test
+    public void savePosts_loadPosts() {
+        // given
+        postsRepository.save(
+            Posts.builder()
+                .title("테스트 게시글")
+                .content("테스트 본문")
+                .author("hjk1019@gmail.com")
+                .build());
+
+        // when
+        List<Posts> postsList = postsRepository.findAll();
+
+        // then
+        Posts posts = postsList.get(0);
+        assertThat(posts.getTitle(), is("테스트 게시글"));
+        assertThat(posts.getContent(), is("테스트 본문"));
+    }
+}
