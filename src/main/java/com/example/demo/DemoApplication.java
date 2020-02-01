434dcb3ab606dc4e51bef48ee384c43fd7d66203
Jinkyu Han <hjk1019@gmail.com>
2020-02-01 17:21:48 +0900

    JPA Auditing을 활성화

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -2,10 +2,19 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+/**
+ * DemoApplication
+ */
+@EnableJpaAuditing	// JPA Auditing 활성화
 @SpringBootApplication
 public class DemoApplication {
 
+	/**
+	 * Main
+	 * @param args arguments
+	 */
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
