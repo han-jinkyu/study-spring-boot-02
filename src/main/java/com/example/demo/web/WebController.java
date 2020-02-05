119e4e2c313cda4094d4aa96c35fea199f899e9f
Jinkyu Han <hjk1019@gmail.com>
2020-02-05 21:45:48 +0900

    Controller에서 모든 게시글 취득 후 모델에 추가하도록 수정

--- src/main/java/com/example/demo/web/WebController.java
+++ src/main/java/com/example/demo/web/WebController.java
@@ -1,6 +1,8 @@
 package com.example.demo.web;
 
+import com.example.demo.service.PostsService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import lombok.AllArgsConstructor;
@@ -13,11 +15,17 @@ import lombok.AllArgsConstructor;
 public class WebController {
 
     /**
+     * 게시글 관련 Service
+     */
+    private PostsService postsService;
+
+    /**
      * 메인페이지로 접속한다
      * @return 메인 페이지 템플릿 이름
      */
     @GetMapping(value = "/")
-    public String main() {
+    public String main(Model model) {
+        model.addAttribute("posts", postsService.findAllDesc());
         return "main";
     }
 }
