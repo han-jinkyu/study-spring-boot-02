fa39c38bf5b12c0704ecfd7c4ea1ba521bc1c9ac
Jinkyu Han <hjk1019@gmail.com>
2020-02-05 21:44:37 +0900

    Repository로부터 모든 게시글을 ID 역순으로 취득하게 수정

--- src/main/java/com/example/demo/domains/posts/PostsRepository.java
+++ src/main/java/com/example/demo/domains/posts/PostsRepository.java
@@ -1,9 +1,19 @@
 package com.example.demo.domains.posts;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.stream.Stream;
 
 /**
  * PostsRepository
  */
 public interface PostsRepository extends JpaRepository<Posts, Long> {
+
+    /**
+     * 모든 게시글를 ID 역순으로 취득하기
+     * @return 역순으로 정렬된 모든 게시글
+     */
+    @Query("SELECT p FROM Posts p ORDER BY p.id DESC")
+    Stream<Posts> findAllDesc();
 }
