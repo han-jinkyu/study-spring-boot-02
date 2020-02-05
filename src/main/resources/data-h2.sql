adc03659dc15e0838bd4db276e6805ca372a4a43
Jinkyu Han <hjk1019@gmail.com>
2020-02-05 21:14:49 +0900

    시작시 들어갈 데이터 설정 및 환경변수 설정

---
+++ src/main/resources/data-h2.sql
@@ -0,0 +1,4 @@
+INSERT INTO posts (title, author, content, created_date, modified_date)
+VALUES
+('테스트1', 'test1@gmail.com', '테스트1의 본문', NOW(), NOW()),
+('테스트2', 'test1@gmail.com', '테스트2의 본문', NOW(), NOW());
